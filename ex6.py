86ad08bffb2bb59464f5de27d15d0e78812a10b7
CalmDownCC <144402238+CalmDownCC@users.noreply.github.com>
2025-03-17 14:47:47 +0800

    Add ex6.py to repository

---
+++ ex6.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age"))
+if age<=19:
+print("you qualify for student discounts")
+elif 20<=age<=54:
+print("you qualify for no age discounts")
+else age>=55:
+print("you can receive senior discounts")
+
